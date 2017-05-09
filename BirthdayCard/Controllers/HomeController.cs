2bc3c16ad7e025815eb48a5621b92213088a0f23
Jeff <Jeff.Peerson@AlaskaAir.com>
2017-05-09 15:31:37 -0700

    Jeff Peerson  - CSharp230  -  Assignment1

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Jeff Peerson
+//CSHP230  MVC  - Assignment 1 - BirthdayCard
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult BirthdayMessage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult BirthdayMessage(Models.BirthdayCard birthdayCard)
+        {
+            return View(birthdayCard);
+        }
+    }
+}
---
+++ BirthdayCard/Models/BirthdayCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+//Jeff Peerson  - CSharp230 - Assignment 1
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter From.")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To.")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Message.")]
+        public string Message { get; set; }
+
+        public bool Complete
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(From) && !string.IsNullOrEmpty(To) && !string.IsNullOrEmpty(Message);
+            }
+        }
+
+    }
+}
